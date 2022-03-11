a4d72d4664d8479d6c4c9de9109ead9ab4003f5a
Horacio <horaciobarrios1989@gmail.com>
2022-03-10 23:55:48 -0300

    Desafio Promise I

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,8 +1,8 @@
 import {useState} from 'react';
 
-function ItemCount() {
-  const stock = 10;
-  const [estado, setEstado] = useState(0);
+function ItemCount({initial, stock}) {
+  
+  const [estado, setEstado] = useState(initial);
   
   const clicSuma =() => {
     if (estado<stock){
@@ -16,28 +16,9 @@ function ItemCount() {
     }
   }
   const clicConfirm =() => {
-    {/*Por fvor revisar!!!
-    
-    <div className="modal fade" id="#confirma" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Confirmado</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div className="modal-body">
-            Su orden ha sido confirmada
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-primary">Ok</button>
-          </div>
-        </div>
-      </div>
-  </div>*/}
-
-  if(estado>0){
+    if(estado>0){
     console.log("Orden confirmada");
-  }else {console.log("Debe seleccionar al menos un elemento");
+    }else {console.log("Debe seleccionar al menos un elemento");
 }
   }
   
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,14 +1,65 @@
 
 import ItemCount from "./ItemCount";
+import { useEffect, useState } from "react"
+
+{/*Simulacion de productos*/}
+let productosIniciales = [
+    {
+        id: 1,
+        nombre: "Producto 1",
+        precio: 100
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        precio: 200
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        precio: 300
+    }
+]
+
 
 const ItemListContainer = () =>{
     
+    const [loading, setLoading] = useState(true)
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        console.log("Ejecutando useEffect")
+        //Aca quiero pedir los productos
+
+        //
+        const pedido = new Promise((res,rej)=>{
+            setTimeout(()=>{
+                res(productosIniciales)
+            },2000)
+        })
+        
+        pedido
+        .then((resultado)=>{
+            console.log("Estuvo bien")
+            setProductos(resultado)
+        })
+        .catch((error)=>{
+            console.log("Estuvo mal")
+        })
+
+    },[])
+
         return (
             <>
             <div className = 'card m-4' id = 'container'>
-            {"Aqui ira el catálogo"}
+            <button onClick={() => setLoading(!loading)}>toggle</button>
+            <ul>
+                {productos.map((producto)=>{
+                    return <li key={producto.id}>{producto.nombre}</li>
+                })}
+            </ul>
             </div>
-            <ItemCount/>
+            <ItemCount stock={10} initial={1}/>
             </>
     )
 }
